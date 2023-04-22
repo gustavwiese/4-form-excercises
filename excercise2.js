3a2119568fbd562eca6e027c206f3f947e801b91
gustavwiese <gusse12345@gmail.com>
2023-04-22 09:31:00 +0200

    done

--- excercise2.js
+++ excercise2.js
@@ -1,7 +1,24 @@
 "use strict";
 
+const redColor = document.querySelector("#red");
+const greenColor = document.querySelector("#green");
+const blueColor = document.querySelector("#blue");
+
 window.addEventListener("load", start);
 
 function start() {
   console.log("Hej");
+  redColor.addEventListener("change", colorChange);
+  greenColor.addEventListener("change", colorChange);
+  blueColor.addEventListener("change", colorChange);
+}
+
+function colorChange() {
+  console.log(`Color value: ${redColor.value}, ${greenColor.value}, ${blueColor.value}`);
+  document.querySelector("#red_label").textContent = `R: ${redColor.value}`;
+  document.querySelector("#green_label").textContent = `G: ${greenColor.value}`;
+  document.querySelector("#blue_label").textContent = `B: ${blueColor.value}`;
+  document.querySelector(
+    "#rgb_block"
+  ).style.backgroundColor = `rgb(${redColor.value}, ${greenColor.value}, ${blueColor.value})`;
 }
